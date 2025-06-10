7db30d2760ae9c162d1f0c2ffd684171f8293781
Mya <Myakay40@hotmail.ca>
2025-06-09 21:55:56 -0400

    Drafted query templates for MyPokemon and Profile

--- SQL/MyPokemon.sql
+++ SQL/MyPokemon.sql
@@ -1,5 +1,4 @@
 --Selects all of the user's caught pokemon instances, along with some relevant species information
-WITH fulldata AS (SELECT * FROM MyPokemon JOIN Pokedex ON MyPokemon.pokemon_id = Pokedex.pid)
 SELECT --user can choose columns, ordering, and filtering (where)
     nickname,
     level,
@@ -7,5 +6,5 @@ SELECT --user can choose columns, ordering, and filtering (where)
     favourite,
     onteam,
     showcase
-FROM fulldata
+FROM MyPokemon JOIN Pokedex ON MyPokemon.pokemon_id = Pokedex.pid
 ORDER BY DateAdded DESC;    
