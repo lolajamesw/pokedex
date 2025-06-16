4200e2757d46a3e69c29f27aead69c26a5e61497
Mya <Myakay40@hotmail.ca>
2025-06-16 19:00:14 -0400

    Wrote general assertions for the number of pokemon a user can have on their team or showcase

---
+++ SQL/ShowcaseSizeCheck.sql
@@ -0,0 +1,10 @@
+--enforces that a user can have no more than 6 pokemon on their team
+CREATE ASSERTION max_6_showcase 
+CHECK NOT EXISTS(
+    SELECT user_id, COUNT(instance_id) AS numTeam FROM(
+        SELECT user_id, instance_id 
+        FROM MyPokemon 
+        WHERE onTeam = 1
+    )
+    WHERE numTeam > 6
+);
