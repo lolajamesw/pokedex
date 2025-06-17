6972b1af95cd50a55c3a48dd2f84306b7be1810a
Mya Kay <myakay38@gmail.com>
2025-06-16 21:11:09 -0400

    Turned pokemon instance id into primary key with for MyPokemon with auto_increment

--- SQL/createTablesQuery.sql
+++ SQL/createTablesQuery.sql
@@ -1,20 +1,20 @@
-DROP TABLE `pokedexbackup`.`Market`;
-DROP TABLE `pokedexbackup`.`Trades`;
-DROP TABLE `pokedexbackup`.`CurrentAttacks`;
-DROP TABLE `pokedexbackup`.`LearnableAttacks`;
-DROP TABLE `pokedexbackup`.`Attacks`;
-DROP TABLE `pokedexbackup`.`MyPokemon`;
-DROP TABLE `pokedexbackup`.`Evolutions`;
-DROP TABLE `pokedexbackup`.`Pokedex`;
-DROP TABLE `pokedexbackup`.`User`;
-DROP TABLE `pokedexbackup`.`TypeFX`;
-DROP TABLE `pokedexbackup`.`Types`;
+DROP TABLE IF EXISTS `pokedex`.`Market`;
+DROP TABLE IF EXISTS `pokedex`.`Trades`;
+DROP TABLE IF EXISTS `pokedex`.`CurrentAttacks`;
+DROP TABLE IF EXISTS `pokedex`.`LearnableAttacks`;
+DROP TABLE IF EXISTS `pokedex`.`Attacks`;
+DROP TABLE IF EXISTS `pokedex`.`MyPokemon`;
+DROP TABLE IF EXISTS `pokedex`.`Evolutions`;
+DROP TABLE IF EXISTS `pokedex`.`Pokedex`;
+DROP TABLE IF EXISTS `pokedex`.`User`;
+DROP TABLE IF EXISTS `pokedex`.`TypeFX`;
+DROP TABLE IF EXISTS `pokedex`.`Types`;
 
-CREATE TABLE pokedexbackup.`Types`(`type` VARCHAR(10) NOT NULL PRIMARY KEY);
+CREATE TABLE pokedex.`Types`(`type` VARCHAR(10) NOT NULL PRIMARY KEY);
 
-CREATE TABLE pokedexbackup.TypeFX(
-	type1 VARCHAR(10) NOT NULL REFERENCES pokedexbackup.`Types`(`type`),
-    type2 VARCHAR(10) NOT NULL REFERENCES pokedexbackup.`Types`(`type`),
+CREATE TABLE pokedex.TypeFX(
+	type1 VARCHAR(10) NOT NULL REFERENCES pokedex.`Types`(`type`),
+    type2 VARCHAR(10) NOT NULL REFERENCES pokedex.`Types`(`type`),
     double_strength BIT NOT NULL,
     half_strength BIT NOT NULL,
     no_impact BIT NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE pokedexbackup.TypeFX(
     CHECK (half_strength+double_strength+no_impact=1)
 );
 
-CREATE TABLE `pokedexbackup`.`User` (
+CREATE TABLE `pokedex`.`User` (
 	`uID` INT NOT NULL PRIMARY KEY,
     `name` VARCHAR(40) NOT NULL,
     `tradeCount` INT NOT NULL,
@@ -32,11 +32,11 @@ CREATE TABLE `pokedexbackup`.`User` (
 		 AND `password` REGEXP '[A-Z]' AND `password` REGEXP '[^a-zA-Z0-9]')
 );
 
-CREATE TABLE `pokedexbackup`.`Pokedex` (
+CREATE TABLE `pokedex`.`Pokedex` (
 	`pID` INT NOT NULL PRIMARY KEY,
     `name` VARCHAR(40) NOT NULL,
-    type1 VARCHAR(10) NOT NULL REFERENCES `pokedexbackup`.`Types`(`type`),
-    type2 VARCHAR(10) REFERENCES `pokedexbackup`.`Types`(`type`),
+    type1 VARCHAR(10) NOT NULL REFERENCES `pokedex`.`Types`(`type`),
+    type2 VARCHAR(10) REFERENCES `pokedex`.`Types`(`type`),
     hp INT NOT NULL,
     atk INT NOT NULL,
     def INT NOT NULL,
@@ -48,28 +48,29 @@ CREATE TABLE `pokedexbackup`.`Pokedex` (
     CHECK (HP >=0 AND atk >= 0 AND def >= 0 AND spAtk >= 0 AND spDef >= 0 AND speed >= 0)
 );
     
-CREATE TABLE `pokedexbackup`.`Evolutions` (
-	pIDfrom INT NOT NULL REFERENCES `pokedexbackup`.`Pokedex`(pID),
-    pIDinto INT NOT NULL REFERENCES `pokedexbackup`.`Pokedex`(pID),
+CREATE TABLE `pokedex`.`Evolutions` (
+	pIDfrom INT NOT NULL REFERENCES `pokedex`.`Pokedex`(pID),
+    pIDinto INT NOT NULL REFERENCES `pokedex`.`Pokedex`(pID),
     PRIMARY KEY(pIDfrom, pIDinto)
 );
-CREATE TABLE pokedexbackup.MyPokemon(
-	pID INT NOT NULL REFERENCES `pokedexbackup`.`Pokedex`(pID),
-    uID INT NOT NULL REFERENCES `pokedexbackup`.`User`(uID),
-    instanceID INT NOT NULL,
+CREATE TABLE pokedex.MyPokemon(
+	pID INT NOT NULL REFERENCES `pokedex`.`Pokedex`(pID),
+    uID INT NOT NULL REFERENCES `pokedex`.`User`(uID),
+    instanceID INT NOT NULL AUTO_INCREMENT,
     nickname VARCHAR(30),
     `level` INT,
     favourite BIT,
     onteam BIT,
     showcase BIT,
-    PRIMARY KEY(pID, instanceID, uID),
+    dateAdded DATETIME,
+    PRIMARY KEY(instanceID),
     CHECK (level>0)
 );
 
-CREATE TABLE pokedexbackup.Attacks(
+CREATE TABLE pokedex.Attacks(
 	aID INT NOT NULL PRIMARY KEY, 
 	attack_name VARCHAR(50) NOT NULL, 
-	`type` VARCHAR(10) NOT NULL REFERENCES pokedexbackup.`Types`(`type`), 
+	`type` VARCHAR(10) NOT NULL REFERENCES pokedex.`Types`(`type`), 
 	category VARCHAR(20), 
 	power INT, 
 	accuracy INT, 
@@ -78,30 +79,28 @@ CREATE TABLE pokedexbackup.Attacks(
     CHECK (power >=0 AND accuracy >= 0 AND PP >= 0)
 );
 
-CREATE TABLE pokedexbackup.LearnableAttacks(
-	pID INT NOT NULL REFERENCES pokedexbackup.Pokedex(pID),
-    aID INT NOT NULL REFERENCES pokedexbackup.Attacks(aID),
+CREATE TABLE pokedex.LearnableAttacks(
+	pID INT NOT NULL REFERENCES pokedex.Pokedex(pID),
+    aID INT NOT NULL REFERENCES pokedex.Attacks(aID),
     PRIMARY KEY (pID, aID)
 );
 
-CREATE TABLE pokedexbackup.CurrentAttacks(
-	pID INT NOT NULL,
+CREATE TABLE pokedex.CurrentAttacks(
     instanceID INT NOT NULL,
-    uID INT NOT NULL,
-    aID INT NOT NULL REFERENCES pokedexbackup.Attacks(aID),
-    PRIMARY KEY (pID, instanceID, uID, aID),
-    FOREIGN KEY (pID, instanceID, uID) REFERENCES pokedexbackup.MyPokemon(pID, instanceID, uID)
+    aID INT NOT NULL REFERENCES pokedex.Attacks(aID),
+    PRIMARY KEY (instanceID, aID),
+    FOREIGN KEY (instanceID) REFERENCES pokedex.MyPokemon(instanceID)
 );
 
 DELIMITER //
 
 CREATE TRIGGER limit_attacks
-BEFORE INSERT ON pokedexbackup.CurrentAttacks
+BEFORE INSERT ON pokedex.CurrentAttacks
 FOR EACH ROW
 BEGIN
 	DECLARE atkCount INT;
     SELECT COUNT(DISTINCT(pID, instanceID, uID)) INTO atkCount
-    FROM pokedexbackup.CurrentAttacks
+    FROM pokedex.CurrentAttacks
     
     IF atkCount >= 4 THEN 
 		SIGNAL SQLSTATE '45000'
@@ -110,26 +109,25 @@ BEGIN
 END
 DELIMITER ;
 
-CREATE TABLE pokedexbackup.Trades(
+CREATE TABLE pokedex.Trades(
 	trade_id INT NOT NULL PRIMARY KEY,
-	seller_pokemon_id INT NOT NULL, 
 	seller_pokemon_instance_id INT NOT NULL, 
 	seller_id INT NOT NULL, 
 	buyer_id INT, 
-	buyer_pokemon_id INT, 
 	buyer_pokemon_instance_id INT, 
 	`status` VARCHAR(15) NOT NULL,
-    FOREIGN KEY (seller_pokemon_id, seller_pokemon_instance_id, seller_id) REFERENCES pokedexbackup.MyPokemon(pID, instanceID, uID)
+    FOREIGN KEY (seller_pokemon_instance_id) REFERENCES pokedex.MyPokemon(instanceID),
+    FOREIGN KEY (seller_id) REFERENCES pokedex.User(uid),
+    FOREIGN KEY (buyer_pokemon_instance_id) REFERENCES pokedex.MyPokemon(instanceID),
+    FOREIGN KEY (buyer_id) REFERENCES pokedex.User(uid)
 );
 
-CREATE TABLE pokedexbackup.Market(
-	offered_pokemon_id INT NOT NULL,
+CREATE TABLE pokedex.Market(
     offered_pokemon_instance_id INT NOT NULL,
     offering_user_id INT NOT NULL,
     request_description VARCHAR(100),
-    reply_pokemon_id INT NOT NULL,
     reply_pokemon_instance_id INT NOT NULL,
     reply_user_id INT NOT NULL,
-	FOREIGN KEY (offered_pokemon_id, offered_pokemon_instance_id, offering_user_id) REFERENCES pokedexbackup.MyPokemon(pID, instanceID, uID),
-	FOREIGN KEY (reply_pokemon_id, reply_pokemon_instance_id, reply_user_id) REFERENCES pokedexbackup.MyPokemon(pID, instanceID, uID)
+	FOREIGN KEY (offered_pokemon_instance_id) REFERENCES pokedex.MyPokemon(instanceID),
+	FOREIGN KEY (reply_pokemon_instance_id) REFERENCES pokedex.MyPokemon(instanceID)
 );
