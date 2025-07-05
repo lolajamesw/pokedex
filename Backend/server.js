9e8a333ca29342c5417de3bdce7d62025bac9119
MahdeeK <mahdee.khandokar@gmail.com>
2025-07-05 19:36:43 -0400

    Added manual entry feature

--- Backend/server.js
+++ Backend/server.js
@@ -9,11 +9,11 @@ app.use(cors())
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'fluffyP23',
-    database: 'pokedex'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 app.get('/', (re, res)=> {
@@ -237,11 +237,11 @@ app.post("/addPokemon", async (req, res) => {
 
   try {
     const dbPromise = await mysqlPromise.createConnection({
-      host: 'localhost',
-      port: 3306,
-      user: 'root',
-      password: 'fluffyP23',
-      database: 'pokedex'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     });
 
     const [rows] = await dbPromise.query(
