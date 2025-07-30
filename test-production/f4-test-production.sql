871e6fb08144d57df053388908819479102bd2d3
Mya Kay <myakay38@gmail.com>
2025-07-29 22:54:12 -0400

    ran production tests

--- test-production/f4-test-production.sql
+++ test-production/f4-test-production.sql
@@ -1,3 +1,3 @@
 -- Fancy Feature 4: User Accounts
 SELECT * FROM User WHERE username= 'User4' AND password= 'Pa$$word4';
-INSERT INTO User('Name101', 0, 'User101', 'Pa$$word101');
+INSERT INTO User(name, tradeCount, username, password) VALUES ('Name101', 0, 'User101', 'Pa$$word101');
